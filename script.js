cf95e13ba07650f5bca0c868015a522d11afe130
bryson5489 <brysonmaul@Brysons-Air.hsd1.mi.comcast.net>
2023-01-11 19:33:26 -0500

    finished project

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("button-finish").addEventListener("click", function () {
+  alert(
+    "Thank you for Submitting your infomation! Someone will be contacting you shortly!😃"
+  );
+});
